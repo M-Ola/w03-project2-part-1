e4fe3f8ee28b9ace7b678f4593307ad010ca001d
ola466@gmail.com <ola466@gmail.com>
2025-11-21 04:21:13 -0600

    Fix url in swagger

--- swagger.js
+++ swagger.js
@@ -1,21 +1,3 @@
-/* const swaggerAutogen = require("swagger-autogen")();
-const doc = {
-  openapi: "3.0.0",
-  info: {
-    title: "Blogs Api",
-    description: "Blogs Api",
-  },
-  host: "localhost:3000",
-  schemes: ["https", "http"],
-};
-
-const outputFile = "./swagger.json";
-const endpointsFiles = ["./routes/index.js"];
-
-//This will generate swagger.json
-swaggerAutogen(outputFile, endpointsFiles, doc);
- */
-
 const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
